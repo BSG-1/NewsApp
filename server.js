9902f72d7c1c4ffb9a9143637a2f65a2dcd20a6f
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-03-11 13:13:56 -0700

    mongoose connects to db

---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+
+mongoose.Promise = Promise;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, {});
+
+app.get("/test", function(req, res) {
+  // TODO: Finish the route so it grabs all of the articles
+  db.Headline.create({'headline': 'test', }, function(err, articles){
+    if (err) {
+      res.json(err)
+    }else{
+      res.json(articles);
+    }
+    
+  })
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
