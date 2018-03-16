17479659ed40adaccb4aee25b51480c6f1340693
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-03-16 14:30:03 -0700

    integrating handlebars

--- models/Headline.js
+++ models/Headline.js
@@ -21,20 +21,10 @@ var HeadlineSchema = new Schema({
     type: String,
     required: true
   },
-  source: {
-    type: String
-  },
-  // saved: {
-  //   type: Boolean,
-  //   default: false
-  // }
-    // `note` is an object that stores a Note id
-    // The ref property links the ObjectId to the Note model
-    // This allows us to populate the Article with an associated Note
-  // note: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: "Note"
-  // }
+  notes: [{
+      type: Schema.Types.ObjectId,
+      ref: "Note"
+    }]
 });
 
 // This creates our model from the above schema, using mongoose's model method
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require('path');
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var request = require("request")
+var request = require("request");
+var hbs = require('express-handlebars');
+var mongojs = require("mongojs");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -12,20 +15,25 @@ var cheerio = require("cheerio");
 
 // Require all models
 var db = require("./models");
-
-var PORT = 3000;
+ 
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
+const routes = require("./routes");
+
+app.use(routes);
 
 // Configure middleware
-
+app.set('views', path.join(__dirname, './views'));
+app.engine('hbs', hbs({extname: '.hbs', defaultLayout: 'main'}));
+app.set('view engine', 'hbs');
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
@@ -47,7 +55,8 @@ app.get("/scrape", function(req, res){
         // console.log(response.data)
         
         // An empty array to save the data that we'll scrape
-        
+        var results = [];
+
         var parentSelector = "article.item";
 
         // // Select each element in the HTML body from which you want information.
@@ -59,8 +68,8 @@ app.get("/scrape", function(req, res){
             results.push({
                 url: $(element).find("a").attr("href"),
                 title: $(element).find("h2.title").text(),
-                blurb: $(element).find("p.teaser").text(),
-            })
+                blurb: $(element).find("p.teaser").text()
+            });
         });
 
         for (let i = 0; i < results.length; i++) {
@@ -71,37 +80,34 @@ app.get("/scrape", function(req, res){
         console.log("saved to DB");
     
         res.send(results);
-    })
+    })    
 });
 
-// 2: All: Send JSON response with all articles
-// app.get("/all", function(req, res) {
-//     // Query: In our database, go to the animals collection, then "find" everything
-//     db.Headlines.find({}, function(err, results) {
-//       // Log any errors if the server encounters one
-//       if (err) {
-//         console.log(err);
-//       }
-//       else {
-//         // Otherwise, send the result of this query to the browser
-//         res.json(results);
-//       }
-//     });
-// });
-// ---------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------//
 
-app.get("/test", function(req, res) {
-  // TODO: Finish the route so it grabs all of the articles
-  db.Headline.create({'headline': 'test', }, function(err, articles){
-    if (err) {
-      res.json(err)
-    }else{
-      res.json(articles);
-    }
-    
-  })
-});
+// app.get("/save-this-article", function(req, res){
+//     console.log("article saved");
+//     db.Note.create(req.body)
+//     .then(function(dbNote) {
+//       // If a Note was created successfully, find one User (there's only one) and push the new Note's _id to the User's `notes` array
+//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+//       return db.Headline.findOneAndUpdate({}, { $push: { notes: dbNote._id } }, { new: true });
+//     })
+//     .then(function(dbUser) {
+//       // If the User was updated successfully, send it back to the client
+//       res.json(dbUser);
+//     })
+//     .catch(function(err) {
+//       // If an error occurs, send it back to the client
+//       res.json(err);
+//     });
+// });
+
+// //display saved articles
+// app.post("/save-this-article", function(req, res){
+
+// });
 
 // Start the server
 app.listen(PORT, function() {
