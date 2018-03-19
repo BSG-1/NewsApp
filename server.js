c9612cd61e20e0b679dbcbf239167875ed6c674a
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-03-18 18:17:24 -0700

    adds most functionality; still needs delete note

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
-
+    useMongoClient: true
 });
 
 //scraping method
